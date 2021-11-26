52edfdad031cbe44c8a2875d7df3811cd55fa0ac
tregubartemofficial <91544488+tregubartemofficial@users.noreply.github.com>
2021-11-26 16:55:35 +0200

    Update задача1.c

--- задача1.c
+++ задача1.c
@@ -1,28 +1,31 @@
-#include <ctype.h>
-#include <cs50.h>
 #include <stdio.h>
-
-int main(void)
+int main()
 {
-    int b = 0; a[n];
-    int n = get_int("Множина чисел: ");
-    for (int i = 0; i < n; i++)
-    {
-        a[i] = get_int("Введіть число %i: ", (i+1));
-        if ((a[i] <= 0) || (a[i] > 100))
-        {
-            b++;
-        }
-    }
-    if (!(b == 0))
-    {
-        printf("Числа повинні бути від 1 до 100");
-    }
-    else
-    {
-        for (int i = n-1; i >= 0; i--)
-        {
-            printf("%i ", a[i]);
-        }
-    }
+  int c; n; d;
+  int a[100];
+  int b[100];
+  printf("Кількість чисел у масиві\n");
+  scanf("%d", &n);
+
+  while (n < 101 && n>0)
+  {
+
+  printf("Введіть масив елементів\n");
+  for (c = 0; c < n ; c++)
+  scanf("%d", &a[c]);
+  // Копіювання елементів в масив
+
+  for (c = n - 1, d = 0; c >= 0; c--, d++)
+  b[d] = a[c];
+
+  for (c = 0; c < n; c++)
+  a[c] = b[c];
+  printf("Оберненний масив:\n");
+
+  for (c = 0; c < n; c++)
+  printf("%d ", a[c]);
+
+  return 0;
+  }
+
 }
